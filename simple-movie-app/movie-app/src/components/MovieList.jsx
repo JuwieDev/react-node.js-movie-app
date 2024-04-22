9365f9cbe014eb71be18592d604e56d5490dde30
Joe <123956110+JuwieDev@users.noreply.github.com>
2024-04-22 11:10:41 +0800

    Add files via upload

---
+++ simple-movie-app/movie-app/src/components/MovieList.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const MovieList = (props) => {
+  const FavouriteComponent = props.favouriteComponent;
+  return (
+    <>
+      {props.movies.map((movie, index) => (
+        <div
+          key={movie.imdbID}
+          className="custom-image-container"
+        >
+          <img src={movie.Poster} alt="movie"></img>
+          <div
+            onClick={()=>props.handleFavouritesClick(movie)}
+            className="custom-overlay"
+          >
+            <FavouriteComponent />
+          </div>
+        </div>
+      ))}
+    </>
+  );
+};
+export default MovieList;
